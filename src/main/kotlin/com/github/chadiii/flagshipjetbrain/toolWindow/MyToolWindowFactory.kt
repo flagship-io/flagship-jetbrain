16f54191c278f4408dd7297dfa2dc50d88c52430
GitHub Action <action@github.com>
2023-09-13 15:25:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/chadiii/flagshipjetbrain/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/chadiii/flagshipjetbrain/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.chadiii.flagshipjetbrain.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.chadiii.flagshipjetbrain.MyBundle
+import com.github.chadiii.flagshipjetbrain.services.MyProjectService
 import javax.swing.JButton
 
 
